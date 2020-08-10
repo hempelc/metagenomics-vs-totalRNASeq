f9401a7338f23c3ee5cd687a637d032743a34558
hempelc <hempelc@uoguelph.ca>
2020-08-10 14:56:43 -0400

    edited script

--- scripts/assembly_pipeline/step_by_step_test_for_loop_script.sh
+++ scripts/assembly_pipeline/step_by_step_test_for_loop_script.sh
@@ -120,6 +120,8 @@ echo -e "Script started with full command: $cmd\n"
 
 echo -e "======== START RUNNING SCRIPT ========\n"
 
+# Save current path in variable to make navigation between directories easier
+base_directory=$(pwd)
 
 ######################### Step 1: trimming ################################
 echo -e "======== START STEP 1: TRIMMING AND ERROR CORRECTION ========\n"
@@ -153,7 +155,7 @@ for trimming_results in step_1_trimming/trimmomatic/*; do
 	mkdir $trimming_results/step_2_rrna_sorting/
 	cd $trimming_results/step_2_rrna_sorting/
 
-	echo -e "======== START STEP 2: rRNA SORTING FOR TRIMMED READS IN FOLDER $trimming_results ========\n"
+	echo -e "======== START STEP 2: rRNA SORTING OF TRIMMED READS IN FOLDER $trimming_results ========\n"
 
 	echo -e "\n======== RUNNING SORTMERNA ========\n"
 	mkdir SORTMERNA/
@@ -164,6 +166,7 @@ for trimming_results in step_1_trimming/trimmomatic/*; do
 	--paired_in -other -fastx 1 \
 	-num_alignments 1 -v \
 	-workdir SORTMERNA/
+  pwd
 	deinterleave_fastq_reads.sh < SORTMERNA/out/aligned.fastq \
 	SORTMERNA/out/aligned_R1.fq SORTMERNA/out/aligned_R2.fq
 	echo -e "\n======== SORTMERNA DONE ========\n"
@@ -197,7 +200,7 @@ for trimming_results in step_1_trimming/trimmomatic/*; do
 	mkdir UNSORTED/
 	cp ../*1P_error_corrected.fastq ../*2P_error_corrected.fastq UNSORTED/
 
-  echo -e "\n======== FINISHED STEP 2: rRNA SORTING FOR TRIMMED READS IN FOLDER $trimming_results ========\n"
+	echo -e "\n======== FINISHED STEP 2: rRNA SORTING OF TRIMMED READS IN FOLDER $trimming_results ========\n"
 
 	######################### Step 3: Assembly ################################
 
@@ -216,8 +219,8 @@ for trimming_results in step_1_trimming/trimmomatic/*; do
 			R1_sorted='UNSORTED/*1P_error_corrected.fastq'
 			R2_sorted='UNSORTED/*2P_error_corrected.fastq'
 		fi
-    pwd
-		echo -e "======== START STEP 3: ASSEMBLY FOR TRIMMED READS IN FOLDER $trimming_results AND rRNA FILTERED READS IN FOLDER $rrna_filter_results/ ========\n"
+
+		echo -e "======== START STEP 3: ASSEMBLY OF TRIMMED READS IN FOLDER $trimming_results AND rRNA FILTERED READS IN FOLDER $rrna_filter_results/ ========\n"
 		mkdir $rrna_filter_results/step_3_assembly/
 		cd $rrna_filter_results/step_3_assembly/
 
@@ -255,9 +258,14 @@ for trimming_results in step_1_trimming/trimmomatic/*; do
 		echo -e "\n======== IDBA_TRAN DONE ========\n"
 
 		echo -e "\n======== RUNNING TRINITY ========\n"
-		Trinity --seqType fq --max_memory 64G --left ../../$R1_sorted --right ../../$R2_sorted \
-    --CPU 16 --output $rrna_filter_results/step_3_assembly/TRINITY/
-		cat TRINITY/Trinity.fasta \
+    if [[ $rrna_filter_results == "rRNAFILTER" ]]; then
+  		Trinity --seqType fa --max_memory 64G --left ../../$R1_sorted --right \
+      ../../$R2_sorted --CPU 16 --output TRINITY/
+    else
+      Trinity --seqType fq --max_memory 64G --left ../../$R1_sorted --right \
+      ../../$R2_sorted --CPU 16 --output TRINITY/
+    fi
+    cat TRINITY/Trinity.fasta \
     | sed 's/ len/_len/g' > TRINITY/Trinity_with_length.fasta
 		echo -e "\n======== TRINITY DONE ========\n"
 
@@ -267,7 +275,7 @@ for trimming_results in step_1_trimming/trimmomatic/*; do
     sed 's/ /_/g' TRANSABYSS/transabyss-final.fa > TRANSABYSS/transabyss-final_edited.fa
 		echo -e "\n======== TRANSABYSS DONE ========\n"
 
-		echo -e "\n======== FINISHED STEP 3: ASSEMBLY FOR TRIMMED READS IN FOLDER $trimming_results AND rRNA FILTERED READS IN FOLDER $rrna_filter_results/ ========\n"
+		echo -e "\n======== FINISHED STEP 3: ASSEMBLY OF TRIMMED READS IN FOLDER $trimming_results AND rRNA FILTERED READS IN FOLDER $rrna_filter_results/ ========\n"
 
 		assembly_results_list="SPADES METASPADES MEGAHIT IDBA_UD RNASPADES IDBA_TRAN TRINITY TRANSABYSS"
 		for assembly_results in $assembly_results_list; do
@@ -289,7 +297,7 @@ for trimming_results in step_1_trimming/trimmomatic/*; do
 				scaffolds='TRANSABYSS/transabyss-final_edited.fa'
 			fi
 
-			echo -e "======== START STEP 4: MAPPING FOR TRIMMED READS IN FOLDER $trimming_results AND rRNA FILTERED READS IN FOLDER $rrna_filter_results/ AND ASSEMBLY IN FOLDER $assembly_results ========\n"
+			echo -e "======== START STEP 4: MAPPING OF TRIMMED READS IN FOLDER $trimming_results AND rRNA FILTERED READS IN FOLDER $rrna_filter_results/ AND ASSEMBLY IN FOLDER $assembly_results/ ========\n"
 			mkdir $assembly_results/step_4_mapping/
 			cd $assembly_results/step_4_mapping/
 
@@ -356,16 +364,47 @@ for trimming_results in step_1_trimming/trimmomatic/*; do
 
 			mv bwa_output.sam merge_input_mapped_bwa.txt merge_input_unmapped_bwa.txt BWA/
 			mv bowtie2_output.sam merge_input_mapped_bowtie2.txt merge_input_unmapped_bowtie2.txt BOWTIE2/
-      echo -e "======== FINISHED STEP 4: MAPPING FOR TRIMMED READS IN FOLDER $trimming_results AND rRNA FILTERED READS IN FOLDER $rrna_filter_results/ AND ASSEMBLY IN FOLDER $assembly_results ========\n"
 
-# DB then classification
+      cd $(realpath --relative-to=$(pwd) ${base_directory}/${trimming_results}/step_2_rrna_sorting/${rrna_filter_results}/step_3_assembly/)
 
+			echo -e "======== FINISHED STEP 4: MAPPING FOR TRIMMED READS IN FOLDER $trimming_results AND rRNA FILTERED READS IN FOLDER $rrna_filter_results/ AND ASSEMBLY IN FOLDER $assembly_results ========\n"
 
-			cd ../../
+			echo -e "======== START STEP 5 AND 6: CLASSIFICATION OF ASSEMBLED SCAFFOLDS FROM TRIMMED READS IN FOLDER $trimming_results AND rRNA FILTERED READS IN FOLDER $rrna_filter_results/ AND ASSEMBLY IN FOLDER $assembly_results ========\n"
+			mkdir $assembly_results/step_5_reference_DB/
+			cd $assembly_results/step_5_reference_DB/
+
+			ref_DB_list="SILVA NCBI_NT"
+			for DB in $ref_DB_list; do
+				if [[ $DB == "SILVA" ]] ; then
+					krakenDB="/hdd1/databases/kraken2_SILVA_DB"
+					#centrifugeDB=XX
+					blastDB="/hdd1/databases/SILVA_database_mar_2020/SILVA_138_SSURef_NR99_tax_silva.fasta"
+				else
+					krakenDB="/hdd1/databases/kraken2_nt_DB"
+					#centrifugeDB=XX
+					blastDB="/hdd1/databases/nt_database_feb_2020_indexed/nt"
+				fi
+
+				mkdir $DB/step_6_classification
+				cd $DB/step_6_classification
+
+				blast_filtering.bash -s ../../$scaffolds -d $blastDB -t soft -T 16
+				mv blast_filtering BLAST_FIRST_HIT
+
+				blast_filtering.bash -s ../../$scaffolds -d $blastDB -t strict -T 16
+				mv blast_filtering BLAST_FILTERED
+
+				# kraken code
+
+				# centrifuge code
+
+				cd $(realpath --relative-to=$(pwd) ${base_directory}/${trimming_results}/step_2_rrna_sorting/${rrna_filter_results}/step_3_assembly/${assembly_results}/step_5_reference_DB/)
+			done
+			cd $(realpath --relative-to=$(pwd) ${base_directory}/${trimming_results}/step_2_rrna_sorting/${rrna_filter_results}/step_3_assembly/)
 		done
-    cd ../
+		cd $(realpath --relative-to=$(pwd) ${base_directory}/${trimming_results}/step_2_rrna_sorting/)
 	done
-	cd ../../../../../
+	cd $(realpath --relative-to=$(pwd) $base_directory)
 done
 
 # Display runtime
