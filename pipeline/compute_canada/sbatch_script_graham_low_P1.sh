94c0261bd86ccbf6bc8ee468a14d44bd9b4d7b7c
hempelc <hempelc@uoguelph.ca>
2021-06-25 15:32:10 -0400

    removed nt DB from graham_low sbatch scripts (not needed there)

--- pipeline/compute_canada/sbatch_script_graham_low_P1.sh
+++ pipeline/compute_canada/sbatch_script_graham_low_P1.sh
@@ -41,7 +41,10 @@ echo -e "Job array ID is ${SLURM_ARRAY_TASK_ID}"
 
 # Copy all necessary DBs and reads to temporary dir on server (SLURM_TMPDIR)
 echo "[$(date +%H:%M:%S)] Copying started [$((($(date +%s)-${start})/3600))h $(((($(date +%s)-${start})%3600)/60))m]"
-cp -r ${BASE}/databases ${BASE}/programs/ete3_env ${HOME}/.etetoolkit \
+cp -r ${BASE}/databases/kraken2_SILVA_138.1_SSU_LSURef_NR99_tax_silva_trunc_DB_Sep_2020 \
+${BASE}/databases/NCBI_staxids*_scientific.txt ${BASE}/databases/SILVA_138.1_SSU_LSURef_NR99_tax_silva_trunc_BLAST_DB_Sep_2020 \
+${BASE}/databases/SILVA_paths_and_taxids.txt ${BASE}/databases/sortmerna_silva_databases \
+${BASE}/programs/ete3_env ${HOME}/.etetoolkit \
 ${R1} ${R2} ${BASE}/split_files_low_P1/file_chunk_${SLURM_ARRAY_TASK_ID} \
 ${BASE}/programs/rRNAFilter ${SLURM_TMPDIR}
 echo "[$(date +%H:%M:%S)] Copying finished [$((($(date +%s)-${start})/3600))h $(((($(date +%s)-${start})%3600)/60))m]"
@@ -49,7 +52,6 @@ echo "[$(date +%H:%M:%S)] Copying finished [$((($(date +%s)-${start})/3600))h $(
 # Set some directory-specific variables
 R1=${SLURM_TMPDIR}/$(basename ${R1})
 R2=${SLURM_TMPDIR}/$(basename ${R2})
-DBS=${SLURM_TMPDIR}/databases
 
 # Activate copies environment
 source ${SLURM_TMPDIR}/ete3_env/bin/activate
@@ -100,21 +102,21 @@ for line in {1..8}; do
 
   METAGENOMICS_METATRANSCRIPTOMICS_PIPELINE_compute_canada.sh \
   -1 ${R1} -2 ${R2} -P ${pipeline} \
-  -N ${DBS}/nt_database_feb_2020_indexed/nt \
-  -S ${DBS}/SILVA_138.1_SSU_LSURef_NR99_tax_silva_trunc_BLAST_DB_Sep_2020/SILVA_138.1_SSU_LSURef_NR99_tax_silva_trunc.fasta \
-  -s ${DBS}/kraken2_SILVA_138.1_SSU_LSURef_NR99_tax_silva_trunc_DB_Sep_2020 \
-  -n ${DBS}/kraken2_nt_DB \
-  -a ${DBS}/sortmerna_silva_databases/silva-arc-16s-id95.fasta \
-  -b ${DBS}/sortmerna_silva_databases/silva-bac-16s-id90.fasta \
-  -e ${DBS}/sortmerna_silva_databases/silva-euk-18s-id95.fasta \
-  -E ${DBS}/sortmerna_silva_databases/silva-euk-28s-id98.fasta \
-  -A ${DBS}/sortmerna_silva_databases/silva-arc-23s-id98.fasta \
-  -B ${DBS}/sortmerna_silva_databases/silva-bac-23s-id98.fasta \
-  -R ${DBS}/sortmerna_silva_databases/rfam-5.8s-database-id98.fasta \
-  -r ${DBS}/sortmerna_silva_databases/rfam-5s-database-id98.fasta \
-  -x ${DBS}/SILVA_paths_and_taxids.txt \
-  -F ${DBS}/NCBI_staxids_scientific.txt \
-  -f ${DBS}/NCBI_staxids_non_scientific.txt \
+  -N ${SLURM_TMPDIR}/nt_database_feb_2020_indexed/nt \
+  -S ${SLURM_TMPDIR}/SILVA_138.1_SSU_LSURef_NR99_tax_silva_trunc_BLAST_DB_Sep_2020/SILVA_138.1_SSU_LSURef_NR99_tax_silva_trunc.fasta \
+  -s ${SLURM_TMPDIR}/kraken2_SILVA_138.1_SSU_LSURef_NR99_tax_silva_trunc_DB_Sep_2020 \
+  -n ${SLURM_TMPDIR}/kraken2_nt_DB \
+  -a ${SLURM_TMPDIR}/sortmerna_silva_databases/silva-arc-16s-id95.fasta \
+  -b ${SLURM_TMPDIR}/sortmerna_silva_databases/silva-bac-16s-id90.fasta \
+  -e ${SLURM_TMPDIR}/sortmerna_silva_databases/silva-euk-18s-id95.fasta \
+  -E ${SLURM_TMPDIR}/sortmerna_silva_databases/silva-euk-28s-id98.fasta \
+  -A ${SLURM_TMPDIR}/sortmerna_silva_databases/silva-arc-23s-id98.fasta \
+  -B ${SLURM_TMPDIR}/sortmerna_silva_databases/silva-bac-23s-id98.fasta \
+  -R ${SLURM_TMPDIR}/sortmerna_silva_databases/rfam-5.8s-database-id98.fasta \
+  -r ${SLURM_TMPDIR}/sortmerna_silva_databases/rfam-5s-database-id98.fasta \
+  -x ${SLURM_TMPDIR}/SILVA_paths_and_taxids.txt \
+  -F ${SLURM_TMPDIR}/NCBI_staxids_scientific.txt \
+  -f ${SLURM_TMPDIR}/NCBI_staxids_non_scientific.txt \
   -t ${SLURM_TMPDIR}/.etetoolkit/taxa.sqlite \
   -T ${EBROOTTRIMMOMATIC}/trimmomatic-0.39.jar \
   -i ${SLURM_TMPDIR}/rRNAFilter
