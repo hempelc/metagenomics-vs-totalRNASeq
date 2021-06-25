91afd246177fc9ce8c829ccb227d3bdedb742a3d
hempelc <hempelc@uoguelph.ca>
2021-06-25 12:27:38 -0400

    edited description and changed pipeline combination generation script

--- pipeline/compute_canada/subscripts/generate_pipeline_combinations_file.py
+++ pipeline/compute_canada/subscripts/generate_pipeline_combinations_file.py
@@ -20,19 +20,27 @@ all=[trimming, sorting, assembly, mapping, db, classification] # list of steps
 combinations_tuples_list=list(itertools.product(*all))
 # Turn that into list of strings:
 combinations_str_list = ['-'.join(tool) for tool in combinations_tuples_list]
-# Split up list in 3 chunks because we run pipelien on 3 servers:
-combinations_chunks=list(split(combinations_str_list, 3))
-combinations_graham=combinations_chunks[0]
-combinations_beluga=combinations_chunks[1]
-combinations_cedar=combinations_chunks[2]
+
+# Write each string in list as line in file:
+with open("combinations_metagenomics_metatranscriptomics_pipeline_all.txt", "w") as file:
+    for x in combinations_str_list:
+        file.write(x + "\n")
+
+
+# # For three servers:
+# # Split up list in 3 chunks because we run pipeline on 3 servers:
+# combinations_chunks=list(split(combinations_str_list, 3))
+# combinations_graham=combinations_chunks[0]
+# combinations_beluga=combinations_chunks[1]
+# combinations_cedar=combinations_chunks[2]
 
 # Write each string in list as line in file for the three servers:
-with open("combinations_metagenomics_metatranscriptomics_pipeline_graham.txt", "w") as file:
-    for x in combinations_graham:
-        file.write(x + "\n")
-with open("combinations_metagenomics_metatranscriptomics_pipeline_beluga.txt", "w") as file:
-    for x in combinations_beluga:
-        file.write(x + "\n")
-with open("combinations_metagenomics_metatranscriptomics_pipeline_cedar.txt", "w") as file:
-    for x in combinations_cedar:
-        file.write(x + "\n")
+# with open("combinations_metagenomics_metatranscriptomics_pipeline_graham.txt", "w") as file:
+#     for x in combinations_graham:
+#         file.write(x + "\n")
+# with open("combinations_metagenomics_metatranscriptomics_pipeline_beluga.txt", "w") as file:
+#     for x in combinations_beluga:
+#         file.write(x + "\n")
+# with open("combinations_metagenomics_metatranscriptomics_pipeline_cedar.txt", "w") as file:
+#     for x in combinations_cedar:
+#         file.write(x + "\n")
