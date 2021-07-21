b97fcb34482f5bd4aa5afa88c47cc42b9a408b60
hempelc <hempelc@uoguelph.ca>
2021-07-21 09:50:24 -0400

    added merge_kraken2.py into script to not switch languages

--- pipeline/compute_canada/METAGENOMICS_METATRANSCRIPTOMICS_PIPELINE_compute_canada_no_ncbi-nt.sh
+++ pipeline/compute_canada/METAGENOMICS_METATRANSCRIPTOMICS_PIPELINE_compute_canada_no_ncbi-nt.sh
@@ -561,23 +561,9 @@ elif [[ $classification == "kraken2" ]]; then
 		| sed '1d' > kraken2_translate_result_edited.txt
 		# Access the SILVA taxonomy file and remove rows with duplicate paths (relict from merging SSU and LSU databases):
 		awk '!a[$1]++' $silva_path_taxid > tmp && mv tmp SILVA_paths_and_taxids.txt && rm tmp
-		# Merge your kraken2 taxids with the SILVA path file to assign a SILVA
-		# taxonomy path to every kraken2 hit:
-		python3 - << EOF # switch to python
-		import pandas as pd
-		df1_name="kraken2_translate_result_edited.txt"
-		df2_name="SILVA_paths_and_taxids.txt"
-		df1 = pd.read_csv(df1_name, sep='\t', names=["tax"])
-		df2 = pd.read_csv(df2_name, sep='\t', names=["tax", "ID"])
-		df3 = pd.merge(df1, df2, left_on="tax", right_on="tax", how='left')
-		df3['ID'].fillna(0, inplace=True)
-		df3=df3.astype({'ID': 'int32'})
-		df4=df3[["ID", "tax"]]
-		df4.to_csv("merged.txt", sep='\t', header=False, index=False)
-
-		EOF
-		# Switched back to bash
-
+		# Merge your kraken2 translated paths with the SILVA path file to assign
+		# a SILVA taxonomy path to every kraken2 hit:
+		merge_kraken2.py
 		sed -i 's/;$//g' merged.txt
 		# Extract the sequence names from the kraken2 output and generate a
 		# final file with sequence name, taxid, and SILVA path:
