06c3fddea58a4de1086603e65b3a95a821908600
hempelc <hempelc@uoguelph.ca>
2022-01-13 16:09:44 -0500

    polished clustering and added pairwise clustering score functions

--- statistics/clustering_env_and_mock_samples.py
+++ statistics/clustering_env_and_mock_samples.py
@@ -5,6 +5,7 @@
 # This script compares clustering of environmental and mock community samples
 
 import pandas as pd
+import numpy as np
 import plotly.express as px
 import os
 import logging
@@ -26,6 +27,10 @@ from scipy.spatial.distance import pdist
 from scipy.spatial.distance import squareform
 from sklearn_extra.cluster import KMedoids
 from skbio.stats.ordination import pcoa
+from itertools import product
+base = importr('base')
+ape = importr('ape')
+
 
 # Activate logging for debugging
 logging.basicConfig(level=logging.DEBUG,
@@ -40,34 +45,65 @@ plotdir = os.path.join(workdir, "cluster_comparison_results")
 if not os.path.exists(plotdir):
     os.mkdir(plotdir)
 ## Set number of clustering tests to estimate appropriate k
-k_test_reps = 2
+k_test_reps = 1 # Note: Kmedoids generates reproducible results
 ## Set number of repetitions for actual clustering to identify clustering with
 ## lowest silhouette score
-k_actual_reps=2
-## Set number of PCs to test in PCAs
-pcs=2
+k_actual_reps=1 # Note: Kmedoids generates reproducible results
 ## Set if you want to loop over all result combinations of genus/species and
 ## rel/pa metrics (True or False)
-looping=True
-## If you set looping to False, then define what specific rank and metrics
+looping=False
+## If you set looping to False, then define what specific rank and metrics and sample
 ## you want to process:
 ### ("genus", "species")
 rank="genus"
 ### ("rel", "pa")
-metrics="pa"
-samples = ["M4_DNA", "M4_RNA", "M5_DNA", "M5_RNA", "M6_DNA", "M6_RNA"]
+metrics="rel"
+samp="M4_RNA"
 
 
 # Parameters set automatically
 if looping:
     groupby_rank_lst=["genus", "species"]
     metr_lst=["rel", "pa"]
+    samples = ["M4_DNA", "M4_RNA", "M5_DNA", "M5_RNA", "M6_DNA", "M6_RNA"]
+
 else:
     groupby_rank_lst=[rank]
     metr_lst=[metrics]
+    samples=[samp]
+
 # Dict that will contain all labels and distance matrices
 labels_master={}
 pairds_master={}
+
+
+# Define functions:
+## Define pdist functions for pairwise ARI and AMI indices
+def dfun_ami(u, v):
+    return adjusted_mutual_info_score(u, v)
+def dfun_ari(u, v):
+    return adjusted_rand_score(u, v)
+
+## Calculate paired CADM using a dictionary of dissimilarity marices
+def paird_cadm(dic):
+    w_lst=[]
+    chi2_lst=[]
+    vals=dic.values()
+    idx=list(dic.keys())
+    len_vals=len(vals)
+    with localconverter(ro.default_converter + pandas2ri.converter):
+        for v, u in product(vals, vals):
+            u_r=base.as_data_frame(pd.DataFrame(u))
+            v_r=base.as_data_frame(pd.DataFrame(v))
+            combined_r=base.rbind(u_r, v_r)
+            ## 4.3 Do CADM in R
+            cadm=ape.CADM_global(combined_r, 2, len(combined_r.columns))
+            # Kendall's coefficient of concordance
+            w_lst.append(cadm[0][0])
+            # Friedman's chi-square statistic
+            chi2_lst.append(cadm[0][1])
+    w_lst_reshaped=np.reshape(w_lst, [len_vals, len_vals])
+    return pd.DataFrame(w_lst_reshaped, index=idx, columns=idx)
 
 
 for metr in metr_lst:
@@ -96,31 +132,30 @@ for metr in metr_lst:
 
                 # Calculate pairwise distance
                 paird=squareform(pdist(df, metric=distance_metric_paird))
-                pairds_master["{0}_{1}_{2}".format(metr, groupby_rank, sample_set)]=paird
+                pairds_master["{0}_{1}_{2}_{3}".format(sample, metr, groupby_rank, sample_set)]=paird
 
 
-                # 2 Clustering based on euclidean distances
-                #   Following https://realpython.com/k-means-clustering-python/ for k-means
-                #   and then adapted code for k-modes for p/a data
+                # 2 Clustering based on dissimilarity matrices
                 ## 2.1 Evaluation of appropriate k
                 k_list=[]
 
-
-                ## We repeat k evaluation 100 times to account for randomness:
+                ## We repeat k evaluation "k_test_reps" times to account for randomness:
                 for rep in range(0, k_test_reps):
                     ## 2.1.1 Estimate the best kluster number k
                     ### Number of k to test
-                    ktest=10
-                    ## 2.1.2 Elbow method based on SSE (kmeans)/cost(kmodes)
-                    ### The dic fit holds the SSE/cost values for each k
+                    ktest=20
+                    ## 2.1.2 Elbow method based on SSE (kmeans)
+                    ### The dic fit holds the SSE values for each k
                     fit={}
                     for k in range(3, ktest):
-                        cluster_alg = KMedoids(n_clusters=k, method="pam", max_iter=1000, metric=distance_metric_kmedoids)
+                        cluster_alg = KMedoids(n_clusters=k, method="pam", max_iter=4000, metric=distance_metric_kmedoids)
                         cluster_alg.fit(paird)
                         fit[k]=(cluster_alg.inertia_)
 
+                    # ### Plot SSEs
                     # fig = px.line(x=fit.keys(), y=fit.values())
                     # fig.show()
+
                     ## 2.1.3 Automatically calculate the best k using KneeLocator
                     kl = KneeLocator(range(3, ktest), list(fit.values()), curve="convex", direction="decreasing")
                     k_list.append(str(kl.elbow))
@@ -128,10 +163,10 @@ for metr in metr_lst:
 
                 ## 2.1.4 Select k
                 ### Count occurence of each tool for each step and save in dict:
-                count_df=pd.DataFrame(Counter(k_list[:-1]), index=["count"]).transpose().reset_index().rename(columns={'index': 'k'})
+                count_df=pd.DataFrame(Counter(k_list), index=["count"]).transpose().reset_index().rename(columns={'index': 'k'})
                 ### Plot counts
-                # fig = px.bar(count_df, x='k', y='count')
-                # fig.show()
+                fig = px.bar(count_df, x='k', y='count')
+                fig.show()
                 ### Select k with highest counts
                 counts=count_df.sort_values("count")["count"].iloc[-1]
                 k=int(count_df.sort_values("count")["k"].iloc[-1])
@@ -143,7 +178,7 @@ for metr in metr_lst:
                 k_actual_dic={}
                 for i in range(0,k_actual_reps):
                     k_act={}
-                    cluster_alg = KMedoids(n_clusters=k, method="pam", max_iter=1000, metric=distance_metric_kmedoids)
+                    cluster_alg = KMedoids(n_clusters=k, method="pam", max_iter=4000, metric=distance_metric_kmedoids)
                     cluster_alg.fit(paird)
                     k_act["silscore"]=silhouette_score(paird, cluster_alg.labels_)
                     k_act["labels"]=cluster_alg.labels_
@@ -157,9 +192,9 @@ for metr in metr_lst:
                     if x["silscore"]==best_silscore:
                         labels=x["labels"]
                 ### And save them for the sample set
-                labels_master["{0}_{1}_{2}".format(metr, groupby_rank, sample_set)]=labels
+                labels_master["{0}_{1}_{2}_{3}".format(sample, metr, groupby_rank, sample_set)]=labels
 
-                # 3 Do PCA with 2 axes
+                # 3 Do PCoA/PCA with 2 axes
                 # PCoA
                 pcoa_model = pcoa(paird, number_of_dimensions=3)
                 pca_graph_df=pcoa_model.samples[['PC1', 'PC2', 'PC3']]
@@ -181,52 +216,30 @@ for metr in metr_lst:
                     "PC2": "PC2 ({0}%)".format(pc2_graph),
                     "PC3": "PC3 ({0}%)".format(pc3_graph)}, symbol="cluster",
                         color="cluster",
-                        title="{0} {1} {2}".format(sample_set, groupby_rank, metr),
+                        title="{0} {1} {2} {3}".format(sample, sample_set, groupby_rank, metr),
                         hover_data=['pipeline'])
                 fig_kmean.show()
 
 
-#LOOP OVER SAMPLES UNSURE
 
-# 3 Calculate Rand index
+# 3 Calculate AMI and ARI
+idx=list(labels_master.keys())
+pairwise_ami= pd.DataFrame(squareform(pdist(list(labels_master.values()), dfun_ami)), index=idx, columns=idx)
+pairwise_ari = pd.DataFrame(squareform(pdist(list(labels_master.values()), dfun_ari)), index=idx, columns=idx)
 
-## Define function to calculate pairwise indices
-def dfun_ami(u, v):
-    return adjusted_mutual_info_score(u, v)
-def dfun_ari(u, v):
-    return adjusted_rand_score(u, v)
+fig_ami=px.imshow(pairwise_ami)
+fig_ami.show()
 
-pairwise_ami, pairwise_ari = pdist(list(labels_master.values()), dfun_ami), pdist(list(labels_master.values()), dfun_ari)
-
-indices.index=list(labels_master["env_samples"].keys())
+fig_ari=px.imshow(pairwise_ari)
+fig_ari.show()
 
 
-# 4 Calculate concordance of distance matrics
-base = importr('base')
-ape = importr('ape')
-## 4.1 Bring dfs in similar shape
-mock_sample_df=samples_set_dfs["mock_samples"]
-env_sample_df=samples_set_dfs["env_samples"]
-for taxon in set(mock_sample_df.columns).difference(env_sample_df.columns):
-    env_sample_df[taxon]=[0]*len(env_sample_df)
-for taxon in set(env_sample_df.columns).difference(mock_sample_df.columns):
-    if taxon not in mock_sample_df.columns:
-        mock_sample_df[taxon]=[0]*len(mock_sample_df)
-mock_sample_df=mock_sample_df.reindex(sorted(mock_sample_df.columns), axis=1)
-env_sample_df=env_sample_df.reindex(sorted(env_sample_df.columns), axis=1)
+# 4 Calculate congruence among distance matrices
+paird_cadm_df=paird_cadm(pairds_master)
 
-## 4.2 Generate distance matrices
-with localconverter(ro.default_converter + pandas2ri.converter):
-    dist_matrix_mock_r=base.as_data_frame(pd.DataFrame(distance_matrix(mock_sample_df, mock_sample_df)))
-    dist_matrix_env_r=base.as_data_frame(pd.DataFrame(distance_matrix(env_sample_df, env_sample_df)))
-    dist_matrix_combined_r=base.rbind(dist_matrix_mock_r, dist_matrix_env_r)
+fig_cadm=px.imshow(paird_cadm_df)
+fig_cadm.show()
 
-    ## 4.3 Do CADM in R
-    cadm=ape.CADM_global(dist_matrix_combined_r, 2, len(dist_matrix_combined_r.columns))
-    # Kendall's coefficient of concordance
-    W=cadm[0][0]
-    # Friedman's chi-square statistic
-    Chi2=cadm[0][1]
 
 
 
