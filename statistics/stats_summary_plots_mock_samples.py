dc2bc0fbe4ac18fe34b874b9b28ef0e2fdb86c05
hempelc <hempelc@uoguelph.ca>
2022-02-07 16:55:01 -0500

    polished and added total count figure

--- statistics/stats_summary_plots_mock_samples.py
+++ statistics/stats_summary_plots_mock_samples.py
@@ -49,12 +49,10 @@ for agg in aggs:
     tool_eval_master_df=pd.DataFrame()
     cluster_counts_all=pd.DataFrame({'tool':tools})
     cluster_counts_master_df=pd.DataFrame()
+    total_counts_master_df=pd.DataFrame()
     plotdir_level2=os.path.join(workdir, "stats_summary_plots", agg)
     if not os.path.exists(plotdir_level2):
         os.mkdir(plotdir_level2)
-
-    test=pd.DataFrame()
-
 
     for level in levels:
         for metr in metr_lst:
@@ -71,8 +69,8 @@ for agg in aggs:
                 #### Set one column with combinations:
                 df["combination"]=["{0}_{1}_{2}".format(combo, level, metr)]*len(df)
                 ### Set two columns to "no" by default:
-                df["mean_euc_dist_lowest"]=["no"]*len(df)
                 df["min_euc_dist_lowest"]=["no"]*len(df)
+                df["mean_euc_dist_lowest"]=["no"]*len(df)
                 df["euc_dist_category"]=["no"]*len(df)
                 for step in steps:
                     #### Replace "mean_euc_dist_lowest" value of lowest mean tool with "yes" for each step
@@ -89,14 +87,22 @@ for agg in aggs:
             for combo, counts in counts_dic.items():
                 counts_dic_res={}
                 for step, dic in counts.items():
-                    for tool, count in dic.items():
-                        counts_dic_res[step + "_" + tool]=count
+                    if step=="mean_euc_dist":
+                        continue
+                    else:
+                        for tool, count in dic.items():
+                            counts_dic_res[step + "_" + tool]=count
                 counts_df=pd.DataFrame(counts_dic_res, index=["counts_abs"]).transpose()\
                     .reset_index().rename(columns={'index': 'tool'})
+                ### Make df for total counts and min euc dist:
+                mean_euc_dist=counts["mean_euc_dist"]
+                total_counts=counts_df[counts_df['tool'].str.contains("type")]["counts_abs"].sum()
+                total_counts_df=pd.DataFrame({'total_counts': total_counts, 'mean_euc_dist': mean_euc_dist}, index=['total_counts'])
                 ### Bring in order and add missing tools:
                 counts_df=pd.merge(cluster_counts_all, counts_df, how='outer').fillna(0)
                 ### Add combinations column:
                 counts_df["combination"]=[combo]*len(counts_df)
+                total_counts_df["combination"]=[combo]
                 ### Generate relative counts for each step
                 counts_df_rel=pd.DataFrame()
                 for step in steps:
@@ -106,9 +112,8 @@ for agg in aggs:
                     sub_df["step"]=[step]*len(sub_df)
                     counts_df_rel=pd.concat([counts_df_rel, sub_df])
                 ### Add to master df
-                #cluster_counts_master_df = pd.concat([cluster_counts_master_df, counts_df_rel])
-                test = pd.concat([test, counts_df_rel])
-
+                cluster_counts_master_df = pd.concat([cluster_counts_master_df, counts_df_rel])
+                total_counts_master_df = pd.concat([total_counts_master_df, total_counts_df])
 
     ## 1.3 Further process tools evaluation master df:
     ### Drop rows with NaN
@@ -129,13 +134,12 @@ for agg in aggs:
     tool_eval_master_df.loc[(tool_eval_master_df["mean_euc_dist_lowest"] == "yes") & (tool_eval_master_df["min_euc_dist_lowest"] == "no") , 'euc_dist_category'] = "mean"
     tool_eval_master_df.loc[(tool_eval_master_df["mean_euc_dist_lowest"] == "no") & (tool_eval_master_df["min_euc_dist_lowest"] == "yes") , 'euc_dist_category'] = "min"
 
+    total_counts_master_df.reset_index(inplace=True)
 
 
     # 2 Graph
     category_orders_tool_eval_tool=[x for x in tools if x in tool_eval_master_df["tool"].unique()]
     category_orders_cluster_counts=[x for x in tools if x in cluster_counts_master_df["tool"].unique()]
-
-
 
     if agg=="agg_reps_agg_type":
         width=1050
@@ -163,6 +167,7 @@ for agg in aggs:
     if cell==False:
         tool_eval_master_df=tool_eval_master_df[~tool_eval_master_df.combination.str.contains("cell")]
         cluster_counts_master_df=cluster_counts_master_df[~cluster_counts_master_df.combination.str.contains("cell")]
+        total_counts_master_df=total_counts_master_df[~total_counts_master_df.combination.str.contains("cell")]
         category_orders_comb=[ x for x in category_orders_comb if "cell" not in x ]
         width=width/1.5
 
@@ -171,8 +176,7 @@ for agg in aggs:
     #tool_eval_master_df["p-value"]=1-tool_eval_master_df["p-value"]
     fig_tool_eval = px.scatter(tool_eval_master_df, x="combination", y="tool",
     	         size="size_lvl", color="euc_dist_category",
-                 hover_name="min_euc_dist", size_max=28, height=1350, width=width,
-                 #symbol="mean_euc_dist_lowest", symbol_sequence=["circle", "circle-dot"],
+                 hover_name="mean_euc_dist", size_max=28, height=1350, width=width,
                  category_orders={"tool": category_orders_tool_eval_tool, "combination": category_orders_comb},
                  color_discrete_map={'none': "lightgrey", 'min': "#0187b3", 'mean': "#e3c284", "both": "#cc0035"})
     fig_tool_eval.update_traces(marker=dict(line=dict(width=0,color='black')), selector=dict(mode='markers'))
@@ -183,19 +187,29 @@ for agg in aggs:
     fig_cluster_counts_abs_rel = px.scatter(cluster_counts_master_df, x="combination", y="tool",
     	         size="counts_rel", color="counts_abs",
                  hover_name="counts_abs", size_max=30, height=1350, width=width,
-                 #symbol="mean_euc_dist_lowest", symbol_sequence=["circle", "circle-dot"],
-                 category_orders={"tool": category_orders_cluster_counts, "combination": category_orders_comb}, color_continuous_scale=["#7c8e00", "#5bdf72", "#edbf0a"])
-                 #color_continuous_scale=px.colors.sequential.haline_r)
-                 #color_discrete_sequence=["lightgrey", "#00ab56", "#dac751", "#737cd0"])
+                 category_orders={"tool": category_orders_cluster_counts, "combination": category_orders_comb}, color_continuous_scale=px.colors.sequential.Viridis)
     fig_cluster_counts_abs_rel.update_traces(marker=dict(line=dict(width=0,color='black')), selector=dict(mode='markers'))
     fig_cluster_counts_abs_rel.write_image(os.path.join(plotdir_level2, agg + "_bubbleplot_cluster_counts_abs_rel.svg"))
     fig_cluster_counts_abs_rel.write_image(os.path.join(plotdir_level2, agg + "_bubbleplot_cluster_counts_abs_rel.png"))
 
-    ## 2.2.2 Cluster counts rel:
+
+    ## 2.2.2 Cluster total counts of each cluster:
+    total_counts_master_df["mean_euc_dist"]=total_counts_master_df["mean_euc_dist"].round(2)
+    fig_cluster_counts_total = px.scatter(total_counts_master_df, x="combination", y="index",
+    	         size="total_counts", color="total_counts",
+                 hover_name="total_counts", size_max=30, width=width,
+                 color_continuous_scale=px.colors.sequential.thermal[0:-2],
+                 category_orders={"combination": category_orders_comb}, text='mean_euc_dist')
+    fig_cluster_counts_total.update_traces(marker=dict(line=dict(width=0,color='black')), selector=dict(mode='markers'))
+    fig_cluster_counts_total.update_traces(textposition='top center')
+    fig_cluster_counts_total.write_image(os.path.join(plotdir_level2, agg + "_bubbleplot_cluster_counts_total.svg"))
+    fig_cluster_counts_total.write_image(os.path.join(plotdir_level2, agg + "_bubbleplot_cluster_counts_total.png"))
+
+
+    ## 2.2.3 Cluster counts rel:
     fig_cluster_counts_rel = px.scatter(cluster_counts_master_df, x="combination", y="tool",
     	         size="counts_rel", color="step",
                  hover_name="counts_rel", size_max=30, height=1350, width=width,
-                 #symbol="mean_euc_dist_lowest", symbol_sequence=["circle", "circle-dot"],
                  category_orders={"tool": category_orders_cluster_counts, "combination": category_orders_comb}, color_discrete_sequence=["#3f47ac",
                     "#ff75b6", "#467600", "#cc0035", "#0187b3", "#01bd7c", "#e3c284"])
     fig_cluster_counts_rel.update_traces(marker=dict(line=dict(width=0,color='black')), selector=dict(mode='markers'))
