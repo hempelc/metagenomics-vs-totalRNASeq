6b2e6762077ac6881282a7a21aa1a2cc4b333df8
hempelc <hempelc@uoguelph.ca>
2022-03-02 16:04:04 -0500

    added best pipeline name extraction and import of subsampled DNA results

--- statistics/abundance_heatmaps.py
+++ statistics/abundance_heatmaps.py
@@ -21,15 +21,16 @@ workdir="/Users/christopherhempel/Desktop/pipeline_results/pipeline_results_mock
 samples = ["M4_DNA", "M4_RNA", "M5_DNA", "M5_RNA", "M6_DNA", "M6_RNA"]
 types=["DNA", "RNA"]
 reps=["M4", "M5", "M6"]
-
+# Include silva, ncbi-nt, or both?
+db='ncbi-nt'
 ## Set if you want to loop over all result combinations of parameters in script
 ## "processing_and_metrics.py" and all metrics (True or False)
 looping=True
 ## If you set looping to False, then define what specific combination
 ## and metrics you want to process:
 ### ("gen_genus", "gen_species")
-combinations=["gen_species"]
-metrics=["pa"]
+combinations=["gen_genus"]
+metrics=["rel"]
 
 # Parameters set automatically
 ## Set lists for aggregation, combinations, and metrics for looping:
@@ -48,17 +49,24 @@ def diff_norm(series):
     diff=abs(series-series["expected"])
     return (diff-diff.min())/(diff.max()-diff.min())
 
+best_pips_master={}
 for combination in combinations:
     for metr in metrics:
         master_df=pd.DataFrame()
+        best_pips={}
         metrics_df_master=pd.DataFrame()
         for sample in samples:
             # Import metrics df
             file=os.path.join(workdir, "metrics_" + combination, sample + "_metrics_df.csv")
             metrics_df=pd.read_csv(file, index_col=0)
+            # Subset df to wanted database
+            if db=='silva':
+                metrics_df=metrics_df[metrics_df['database']!='ncbi-nt']
+            elif db=='ncbi':
+                metrics_df=metrics_df[metrics_df['database']!='silva']
             metrics_df["sample"]=[sample]*len(metrics_df)
             metrics_df_master=pd.concat([metrics_df_master, metrics_df])
-        # Drop unwanted columns and standardize P/A if applicable
+        # Drop unwanted columns
         if metr=="rel":
             metrics_df_master=metrics_df_master.drop(metrics_df_master.loc[:,'TP':'classifier'], axis=1)
         elif metr=="pa":
@@ -74,15 +82,38 @@ for combination in combinations:
             metrics_df_master_no_exp.loc[index,'euc_dist'] = euclidean(row.drop(["pipeline", "sample"]), exp)
         metrics_df_master_no_exp=metrics_df_master_no_exp.set_index("pipeline")
         for sample in samples:
-            # Pick best pipeline metrics based on Euclidean distance
+            # Pick best pipeline metrics based on Euclidean distance and collect
+            # name of best pipelines in best_pips_master
             subdf=metrics_df_master_no_exp[metrics_df_master_no_exp['sample']==sample].drop(['sample'], axis=1)
-            best=subdf.sort_values('euc_dist').iloc[0]\
-                .rename("_".join(sample.split("_")[::-1]))
-            master_df=master_df.append(best)
+            best=subdf.sort_values('euc_dist').iloc[0]
+            best_dist=subdf.sort_values('euc_dist')['euc_dist'][0]
+            best_pipelines=subdf[subdf['euc_dist']==best_dist].index
+            master_df=master_df.append(best.rename("_".join(sample.split("_")[::-1])))
+            best_pips[sample]=list(best_pipelines)
+        best_pips_master[combination + "_" + metr]=best_pips
         # Add expected and sort
         master_df=master_df.append(exp).fillna(0).sort_values('expected', axis=1, ascending=False)
         master_df=master_df.reindex(["DNA_M4", "DNA_M5", "DNA_M6", "expected", "RNA_M4", "RNA_M5", "RNA_M6"])
         master_df=master_df[[c for c in master_df if c!='euc_dist'] + ['euc_dist']]
+
+        # Until here, the code was based on the initial pipeline results based on all reads.
+        # But we had to downsample DNA reads to make DNa and RNA comparable.
+        # Therefore, we import data from subsampled DNA samples and replace DNA metrics above:
+        subsamples = ["M4_DNA_subsample", "M5_DNA_subsample", "M6_DNA_subsample"]
+        subsample_dir="/Users/christopherhempel/Desktop/pipeline_results/pipeline_results_mock_samples_DNA_subsample"
+        for subsample in subsamples:
+            subsample_file=os.path.join(subsample_dir, "{0}_{1}_{2}_metrics_df.csv".format(subsample, combination.replace("gen_",""), metr))
+            subsample_metrics_df=pd.read_csv(subsample_file, index_col=0).drop("expected")
+            if metr=="rel":
+                subsample_metrics_df=subsample_metrics_df.drop(subsample_metrics_df.loc[:,'TP':'FP'], axis=1)
+                subsample_metrics=subsample_metrics_df.mean(axis=0)
+            elif metr=="pa":
+                subsample_metrics_df=subsample_metrics_df.loc[:, ["TP","FP"]]
+                subsample_metrics=subsample_metrics_df.mean(axis=0).round(0)
+            subsample_metrics["euc_dist"]=euclidean(subsample_metrics, exp)
+            name="_".join(subsample.replace("_subsample","").split("_")[::-1])
+            master_df.loc[name]=subsample_metrics
+
 
         # Normalize abundances from 0 to 1
         master_df_norm_w_exp=master_df.apply(diff_norm)
