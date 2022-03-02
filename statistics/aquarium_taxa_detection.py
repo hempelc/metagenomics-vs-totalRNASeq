63e095dbc7220406dde9653bd9dbac5385841df1
hempelc <hempelc@uoguelph.ca>
2022-03-02 16:52:44 -0500

    added comments

--- statistics/aquarium_taxa_detection.py
+++ statistics/aquarium_taxa_detection.py
@@ -4,12 +4,12 @@
 
 # This script tests if expected genera and species are found among aquarium samples
 
-
 import pandas as pd
 
 workdir='/Users/christopherhempel/Desktop/pipeline_results/pipeline_results_env_samples/'
 
 samples=["F4_DNA", "F4_RNA", "F5_DNA", "F5_RNA", "F6_DNA", "F6_RNA"]
+# Expected taxa
 genera=["Caridina", "Ostracoda", "Aplocheilus", "Cynodonichthys", "Danio", "Poecillia", "Anubias",
     "Bolbitis", "Cryptocoryne", "Echinodoras", "Limnophila", "Rotala", "Sagittaria",
     "Vesicularia", "Physella", "Planorbarius", "Melanoides", "Neritidae"]
@@ -21,6 +21,7 @@ species=["Caridina multidentata", "Aplocheilus lineatus", "Cynodonichthys hildeb
 df_genus=pd.DataFrame()
 df_spe=pd.DataFrame()
 
+# Generate dics with df of respective rerun_dna_subsamples
 gen_dic={}
 for sample in samples:
     df=pd.read_csv(workdir + "rel_abun_genus/" + sample + '_rel_abun_genus_pa.csv')
@@ -31,6 +32,7 @@ for sample in samples:
     df=pd.read_csv(workdir + "rel_abun_species/" + sample + '_rel_abun_species_pa.csv')
     spe_dic[sample]=df
 
+# Indicate in dfs if taxa were found (yes/no)
 for genus in genera:
     lst=[]
     for df in gen_dic.values():
