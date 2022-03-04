0f3e1c3b5a638f72d4e82f57558fba100c06ce77
hempelc <hempelc@uoguelph.ca>
2022-03-04 12:29:29 -0500

    added improvements so that missing data can be handled

--- statistics/subsample_curves.py
+++ statistics/subsample_curves.py
@@ -4,18 +4,18 @@
 
 # This script generates performance curves for various subsample sizes of DNA and RNA samples
 
-import pandas as pd
 import os
+import pandas as pd
 import numpy as np
-from scipy.spatial.distance import euclidean
 import plotly.graph_objs as go
+from scipy.spatial.distance import euclidean
 
 
 # Parameters set manually
 samples = ["M4_DNA", "M4_RNA", "M5_DNA", "M5_RNA", "M6_DNA", "M6_RNA"]
 workdir = "/Users/christopherhempel/Desktop/pipeline_results/pipeline_results_mock_samples_subsamples_curves/"
 ## Subsample read numbers
-subsample_readnums=[20000, 40000, 60000, 78149]
+subsample_readnums=[1000, 2500, 5000, 10000, 20000, 40000, 60000, 78149]
 ## Indicate if you want to keep replicates separate
 sep_reps=False
 ## Indicate if you want to loop over all 4 combinations of genus/species and cell/gen (True/False)
@@ -24,7 +24,7 @@ looping=True
 ## you want to process:
 rank="genus"
 database="silva"
-dt_type="pa"
+dt_type="rel"
 
 
 # Parameters set automatically
@@ -48,7 +48,8 @@ for sample in samples:
             for db in db_lst:
 
                 # Empty storage df
-                df_eucdist=pd.DataFrame()
+                subsamples=[sample + "_subsample_" + str(x) for x in range(1,11)]
+                df_eucdist=pd.DataFrame({}, index=subsamples)
 
                 # Loop over subsample size
                 for subsample_readnum in subsample_readnums:
@@ -62,13 +63,16 @@ for sample in samples:
                         df=df.loc[:, ["TP","FP"]]
                     # Separate expected from df
                     df_no_exp=df.drop(["expected"], axis=0)
+                    # If df is empty, then skip
+                    if df_no_exp.empty:
+                        continue
                     exp=df.loc["expected"]
                     # Calculate euc dist for each pipeline
                     for index,row in df_no_exp.iterrows():
                         df_no_exp.loc[index,'euc_dist'] = euclidean(row, exp)
                     # Add to df and add df to master dic
-                    df_eucdist[str(subsample_readnum)]=df_no_exp['euc_dist']
-                    master_df["{0}_{1}_{2}_{3}".format(sample, db, groupby_rank, data_type)]=df_eucdist
+                    df_eucdist[subsample_readnum]=df_no_exp['euc_dist']
+                master_df["{0}_{1}_{2}_{3}".format(sample, db, groupby_rank, data_type)]=df_eucdist
 
 for na in ["RNA", "DNA"]:
     for groupby_rank in groupby_rank_lst:
@@ -82,6 +86,7 @@ for na in ["RNA", "DNA"]:
                 master_df_no_reps["{0}_{1}_{2}_{3}".format(na, db, groupby_rank, data_type)]=rep_df
 
 # Plot (taken from https://plotly.com/python/continuous-error-bars/)
+## Make a plot for each evaluation level and add each sampel as layer in a for loop
 for groupby_rank in groupby_rank_lst:
     for data_type in data_types:
         for db in db_lst:
@@ -91,37 +96,45 @@ for groupby_rank in groupby_rank_lst:
             else:
                 dic=master_df_no_reps
             lvls=[x for x in dic.keys() if "{0}_{1}_{2}".format(db, groupby_rank, data_type) in x]
+            ## Sort by DNA and RNA
+            lvls=[x for x in lvls if "DNA" in x]+[x for x in lvls if "RNA" in x]
             for lvl in lvls:
                 if "RNA" in lvl:
-                    col='rgba(255,0,0,1)'
-                    col_err='rgba(255,0,0,0.2)'
+                    color='rgba(220,50,32,1)'
+                    color_err='rgba(220,50,32,0.1)'
                 else:
-                    col='rgba(0,0,255,1)'
-                    col_err='rgba(0,0,255,0.2)'
-                # # Calculate mean and sd
+                    color='rgba(0,90,181,1)'
+                    color_err='rgba(0,90,181,0.1)'
+                name=lvl.replace("_{0}_{1}_{2}".format(db, groupby_rank, data_type), '')
+                ## Calculate mean and sd
                 mean=dic[lvl].mean()
-                sd=dic[lvl].std()
+                sd=pd.Series()
+                for col in dic[lvl].columns:
+                    col_no_nan=dic[lvl][col].dropna()
+                    if len(col_no_nan)==1:
+                        sd.loc[col]=0
+                    else:
+                        sd.loc[col]=dic[lvl][col].dropna().std()
                 mean_plus_sd=mean+sd
                 mean_minus_sd=mean-sd
                 fig.add_trace(
                     go.Scatter(
-                        name=lvl,
-                        x=subsample_readnums,
+                        name=name,
+                        x=list(dic[lvl].columns),
                         y=mean,
-                        line=dict(color=col),
-                        mode='lines'))
+                        line=dict(color=color),
+                        mode='lines+markers'))
                 fig.add_trace(
                     go.Scatter(
-                        x=subsample_readnums+subsample_readnums[::-1], # x, then x reversed
+                        x=list(dic[lvl].columns)+list(dic[lvl].columns)[::-1], # x, then x reversed
                         y=list(mean_plus_sd)+list(mean_minus_sd)[::-1], # upper, then lower reversed
                         fill='toself',
-                        fillcolor=col_err,
+                        fillcolor=color_err,
                         line=dict(color='rgba(255,255,255,0)'),
                         hoverinfo="skip",
                         showlegend=False))
-            fig.update_layout(
-            title="{0}_{1}_{2}".format(db, groupby_rank, data_type),
-            xaxis_title="Number of reads",
-            yaxis_title="Euclidean distance to reference",
-            legend_title="Sample")
+            fig.update_layout(title="{0}_{1}_{2}".format(db, groupby_rank, data_type),
+                xaxis_title="Number of reads",
+                yaxis_title="Euclidean distance to reference",
+                legend_title="Sample")
             fig.show()
