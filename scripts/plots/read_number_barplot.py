d532b3556db3c69e011600d30a42025da683a13d
hempelc <hempelc@uoguelph.ca>
2023-05-06 12:48:40 -0400

    added aquarium samples

--- scripts/plots/read_number_barplot.py
+++ scripts/plots/read_number_barplot.py
@@ -12,20 +12,32 @@ import plotly.express as px #v5.5.0
 outdir="/Users/christopherhempel/Desktop/"
 
 # Specify number of reads
-reads = {"DNA 1 R1": 817619, "DNA 1 R2": 817619, "RNA 1 R1": 94633,
-    "RNA 1 R2": 94633, "DNA 2 R1": 644634, "DNA 2 R2": 644634, "RNA 2 R1": 78149,
-    "RNA 2 R2": 78149, "DNA 3 R1": 669382, "DNA 3 R2": 669382, "RNA 3 R1": 120144,
-    "RNA 3 R2": 120144, "DNA ExtCon R1": 399, "DNA ExtCon R2": 399,
-    "RNA ExtCon R1": 887, "RNA ExtCon R2": 887, "DNA FilCon R1": 640,
-    "DNA FilCon R2": 640, "RNA FilCon R1": 1551, "RNA FilCon R2": 1551}
+reads = {"Mock DNA 1 R1": 817619, "Mock DNA 1 R2": 817619, "Mock RNA 1 R1": 94633,
+    "Mock RNA 1 R2": 94633, "Mock DNA 2 R1": 644634, "Mock DNA 2 R2": 644634, "Mock RNA 2 R1": 78149,
+    "Mock RNA 2 R2": 78149, "Mock DNA 3 R1": 669382, "Mock DNA 3 R2": 669382, "Mock RNA 3 R1": 120144,
+    "Mock RNA 3 R2": 120144, "Mock DNA ExtCon R1": 399, "Mock DNA ExtCon R2": 399,
+    "Mock RNA ExtCon R1": 887, "Mock RNA ExtCon R2": 887, "Mock DNA FilCon R1": 640,
+    "Mock DNA FilCon R2": 640, "Mock RNA FilCon R1": 1551, "Mock RNA FilCon R2": 1551,
+    "Aquarium DNA 1 R1": 1355159, "Aquarium DNA 1 R2": 1355159, "Aquarium RNA 1 R1": 1902388,
+    "Aquarium RNA 1 R2": 1902388, "Aquarium DNA 2 R1": 1373310, "Aquarium DNA 2 R2": 1373310, "Aquarium RNA 2 R1": 1099851,
+    "Aquarium RNA 2 R2": 1099851, "Aquarium DNA 3 R1": 1571705, "Aquarium DNA 3 R2": 1571705, "Aquarium RNA 3 R1": 773067,
+    "Aquarium RNA 3 R2": 773067, "Aquarium DNA ExtCon R1": 99, "Aquarium DNA ExtCon R2": 99,
+    "Aquarium RNA ExtCon R1": 2685, "Aquarium RNA ExtCon R2": 2685, "Aquarium DNA FilCon R1": 157,
+    "Aquarium DNA FilCon R2": 157, "Aquarium RNA FilCon R1": 1137, "Aquarium RNA FilCon R2": 1137}
 
 # Specify percentage of duplictes
-percentage = {"DNA 1 R1": 6.0, "DNA 1 R2": 5.5, "RNA 1 R1": 74.1,
-    "RNA 1 R2": 74.5, "DNA 2 R1": 4.8, "DNA 2 R2": 4.3, "RNA 2 R1": 70.8,
-    "RNA 2 R2": 68.0, "DNA 3 R1": 4.6, "DNA 3 R2": 4.3, "RNA 3 R1": 77.1,
-    "RNA 3 R2": 77.0, "DNA ExtCon R1": 1.5, "DNA ExtCon R2": 0.3,
-    "RNA ExtCon R1": 64.9, "RNA ExtCon R2": 5.3, "DNA FilCon R1": 12.3,
-    "DNA FilCon R2": 7.0, "RNA FilCon R1": 74.7, "RNA FilCon R2": 8.5}
+percentage = {"Mock DNA 1 R1": 6.0, "Mock DNA 1 R2": 5.5, "Mock RNA 1 R1": 74.1,
+    "Mock RNA 1 R2": 74.5, "Mock DNA 2 R1": 4.8, "Mock DNA 2 R2": 4.3, "Mock RNA 2 R1": 70.8,
+    "Mock RNA 2 R2": 68.0, "Mock DNA 3 R1": 4.6, "Mock DNA 3 R2": 4.3, "Mock RNA 3 R1": 77.1,
+    "Mock RNA 3 R2": 77.0, "Mock DNA ExtCon R1": 1.5, "Mock DNA ExtCon R2": 0.3,
+    "Mock RNA ExtCon R1": 64.9, "Mock RNA ExtCon R2": 5.3, "Mock DNA FilCon R1": 12.3,
+    "Mock DNA FilCon R2": 7.0, "Mock RNA FilCon R1": 74.7, "Mock RNA FilCon R2": 8.5,
+    "Aquarium DNA 1 R1": 2.9, "Aquarium DNA 1 R2": 2.6, "Aquarium RNA 1 R1": 58.6,
+    "Aquarium RNA 1 R2": 55.9, "Aquarium DNA 2 R1": 2.3, "Aquarium DNA 2 R2": 2.1, "Aquarium RNA 2 R1": 55.2,
+    "Aquarium RNA 2 R2": 52.2, "Aquarium DNA 3 R1": 2.8, "Aquarium DNA 3 R2": 2.5, "Aquarium RNA 3 R1": 53.6,
+    "Aquarium RNA 3 R2": 51.4, "Aquarium DNA ExtCon R1": 1, "Aquarium DNA ExtCon R2": 0,
+    "Aquarium RNA ExtCon R1": 47.6, "Aquarium RNA ExtCon R2": 6.3, "Aquarium DNA FilCon R1": 3.8,
+    "Aquarium DNA FilCon R2": 0.6, "Aquarium RNA FilCon R1": 74.6, "Aquarium RNA FilCon R2": 7.7}
 
 # Make df
 reads_df=pd.DataFrame([reads, percentage], index=["reads", "dupl_perc"]).transpose().reset_index()
